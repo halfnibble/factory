5c14ef3f6844e2420063ab4710d3e22932f0f235
halfnibble <josh@halfnibble.com>
2015-06-02 23:27:06 -0700

    Add admin, then add sample data. Bug fixes.

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Car, Tires
 
-# Register your models here.
+
+class CarAdmin(admin.ModelAdmin):
+    pass
+
+
+class TiresAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Tires, TiresAdmin)
--- cars/models.py
+++ cars/models.py
@@ -5,10 +5,20 @@ class Tires(models.Model):
     name = models.CharField("Brand Name", max_length=40)
     size = models.FloatField("Tire R", default=17.0)
 
+    def __str__(self):
+        return u"{0}-R{1}".format(self.name, self.size)
+
+    class Meta:
+        verbose_name_plural = "Tires"
+
+
 
 class Car(models.Model):
     """Cars are the main thing our factory products."""
     name = models.CharField("Car Model", max_length=40)
-    year = models.IntegerField("Year", max_length=4, default=2015)
+    year = models.IntegerField("Year", default=2015)
     color = models.CharField("Color", max_length=20, default="White")
     tires = models.ForeignKey(Tires)
+
+    def __str__(self):
+        return self.name
